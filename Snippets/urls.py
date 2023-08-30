cdee960a9755fec15ecc93d06179f4c93220788a
AlekseyVeselobor <ovsaleksei@yandex.ru>
2023-08-30 20:56:22 +0300

    prepare to deploy

---
+++ Snippets/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from MainApp import views
+from django.contrib import admin
+
+
+urlpatterns = [
+    path("admin", admin.site.urls),
+    path('', views.index_page, name='home'),
+    path('snippets/add', views.add_snippet_page, name='snippets-add'),
+    path('snippets/list', views.snippets_page, name='snippets-list'),
+    path('snippet/<int:snippet_id>', views.snippet_detail, name='snippet-detail'),
+    path('snippet/<int:snippet_id>/delete', views.snippet_delete, name='snippet-delete'),
+    path('snippet/<int:snippet_id>/edit', views.snippet_edit, name='snippet-edit'),
+    path('snippet/my', views.my_snippets, name='my-snippets'),
+    path('auth/register', views.create_user, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
